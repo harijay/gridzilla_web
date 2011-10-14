05ca4342907b13a7621edee612854a5c3cd7506d
hari <harijay@gmail.com>
2011-10-13 23:48:38 -0400

    Initial fab commit with fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,9 @@
 __author__ = 'hari'
 from fabric.api import local
 
-def prepare_deploy():
-    local("git add *")
+def prepare_deploy(message):
+    local("git add index.html")
+    local("git add gridzilla_main.css")
+    local("git add fabfile.py")
+    local("""git commit -m "%s" """ % message)
   
